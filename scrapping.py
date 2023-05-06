ee3bd7d5718c5d4f9a6fdc67145d0ec936624005
BoulemiaBilel <bilelboulemia3@gmail.com>
2023-05-06 19:52:12 +0200

    bug au niveau des expressions régulières

--- scrapping.py
+++ scrapping.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import re
+
 
 liens = []                                                          
 nom_organismes = [] 
 descriptions = []
+villes = []
 
+"""
 for i in range (0, 32, 15):
     url = 'https://www.emploilr.com/formation/annuaire/' + str(i) +'.html'
     print(url)
@@ -25,6 +29,12 @@ for i in range (0, 32, 15):
     
         for cda in nom_organismes_a:
             nom_organismes.append(cda.text.strip())
+
+        #Villes organismes
+
+        div_villes = html.find_all('div',{'class':'card bg-blue'})
+        for div_ville in div_villes:
+            villes.append(div_ville.find('span').text)
     
 
         #Desciptions oganismes
@@ -38,7 +48,7 @@ for i in range (0, 32, 15):
         for i in range (len(descriptions)):
             descriptions[i] = descriptions[i].replace('\r','').replace('\n','')
     
-        #print(descriptions)
+        
         liens_orga = []
         for cda in card_info:
             liens_orga.append(cda.find('a',{'class':'card__link card__link--sm'}))
@@ -48,11 +58,40 @@ for i in range (0, 32, 15):
 
         time.sleep(3)
 
-print(nom_organismes)
-print(len(liens))
-print(len(descriptions))
-    
+with open('urls.txt', 'w') as file:
+    for lien in liens:
+        file.write(lien + '\n')
+"""
+code_postal = []
+adresses = []
+telephones = []
 
-#il me reste à parcourir les liens pour avoir les infos tel l'adresse le CP, le numéro de tel le mail et le nom du contact
-        
 
+with open('urls.txt', 'r') as file:
+    for row in file:
+        url = row.strip()
+
+        reponse = requests.get(url)
+        if reponse.ok:
+            html = BeautifulSoup(reponse.text, features = "html.parser")
+
+            divcardbody = html.find_all('div',{'class':'business-card__body'})
+            for p in divcardbody:
+                infos = []
+                infos.append(p.text)
+                adresse_regex = r'\d{1,3}\s[\w\s]+\n'
+                code_postal_regex = r'\d{5}'
+                telephone_regex = r'Tel\s:\s(\d{2}\.\d{2}\.\d{2}\.\d{2})'
+
+                adresse = re.search(adresse_regex, infos[0]).group(0).strip()
+                codepostal = re.search(code_postal_regex, infos[0]).group(0)
+                telephone = re.search(telephone_regex, infos[0]).group(1)
+
+                code_postal.append(codepostal)
+                adresses.append(adresse)
+                telephones.append(telephone)
+
+            
+print (code_postal)
+print (adresses)
+print (telephones)
